c3e5f45684bec2ff849493548ae20dd3d9dba22f
Abulkalam1524 <azad93204@gmail.com>
2026-07-28 04:13:12 +0530

    Add network scanner script

---
+++ network_scanner.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+import optparse
+import os
+import sys
+
+def check_root():
+    if os.geteuid() != 0:
+        print("[-] Please run this program with root privileges (use sudo).")
+        sys.exit(1)
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
+    options, arguments = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a target IP or IP range, use --help for more info.")
+    return options
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+check_root()
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
